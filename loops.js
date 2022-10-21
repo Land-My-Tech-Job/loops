fdc36d9a26fc0a175d188fb0a1cf5952c4ded0c3
LMTJ-Teacher <116300715+LMTJ-Teacher@users.noreply.github.com>
2022-10-20 20:40:46 -0400

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,35 @@
+// Problem One
+
+// Print the numbers from 0 to 15
+
+// Problem Two
+
+// Write a JavaScript loop that will iterate from 0 to 15. For each iteration it will check if the current number is odd or even and display a message on the screen
+
+// Problem Three
+
+// Print the ODD numbers from 7 to 31
+
+// Problem Four
+
+// Print the EVEN numbers from 10 to -20
+
+// Problem Five
+
+// Iterate through all numbers from 1 to 45. Print the following:
+
+// For multiples of 3 print “Fizz”
+// For multiples of 5 print “Buzz”
+// For multiples of 3 and 5 print “FizzBuzz”
+
+// Problem Six
+
+// Print all the elements of the following array
+
+let thisIsAnArray = ["element1", "element2", "element3", "element4"];
+
+// Problem Seven
+
+// Calculate the sum of all the numbers in the following array
+
+let numbersArray = [1,13,22,123,49]
