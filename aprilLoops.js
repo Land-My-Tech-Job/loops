1a1ad91c9d05223fdcf0607661b96c13dba0a02f
Demond Harris <Dae@demonds-mbp.myfiosgateway.com>
2023-02-22 01:57:38 -0500

    Loop Updates

--- aprilLoops.js
+++ aprilLoops.js
@@ -2,3 +2,41 @@
 //for (let i = 0; i <= 15; i++) {
     //console.log(i);
 //}
+//Problem Two
+//let num = 0;
+//for (let i = 0; i <= 15; i++) {
+    
+//}
+//while (num <= 15) {
+    //if (num % 2 === 0) {
+        //console.log(num + " " + 'This number is even');
+    //}
+    //num++;
+//}
+
+//Problem Three
+//let num = 7;
+//for (let i = 0; i <= 31; i++) {
+
+//}
+//while (num <= 31) {
+    //if (num % 2 !== 0) {
+        //console.log(num);
+    //}
+    //num++;
+//}
+
+//Problem Four
+//let num = -20
+//for (let i = 0; i <= 20; i++) {
+
+//}
+//while (num <= 10) {
+    //if (num % 2 === 0) {
+        //console.log(num);
+    //}
+        // num++;
+    //}
+
+    //Problem Five
+    
